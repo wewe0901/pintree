3f62dde37cc97a1da1d5539cd2d1c4fc85c793a7
wewe0901 <wewe0901@gmail.com>
2012-08-02 15:10:06 +0800

    add datepicker

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ exports.show = function(req, res) {
     console.log(req.param.id); 
     Message.findOne({_id:req.params.id}, function(err, message) {
         var today = new Date();
+        console.log("show view : "+ message);
         if(message.publicDate !=null && message.publicDate > today)
             res.render('futureMessage',{title:'pinTree',message:message});
         else
@@ -25,10 +26,11 @@ exports.show = function(req, res) {
 
 exports.post = function(req, res) {
     var data =req.body.publicDate.split('/');
-    var day = parseInt(data[0]);
-    var month = parseInt(data[1]);
-    var year = parseInt(data[2]);
-    console.log(req.body.publicDate + ' -> '+year+'-'+month+'-'+day);
+    var month = parseInt(data[0],10) - 1; // javascript date use 0~11 to present month
+    var day = parseInt(data[1],10);
+    var year = parseInt(data[2],10);
+    console.log('post data :'+req.body.publicDate + ' -> '+year+'-'+month+'-'+day);
+    console.log(' new Date :' + new Date(year,month,day));
     new Message({title:req.body.title,message:req.body.message,publicDate: new Date(year, month, day)}).save();
     res.redirect('back');
 };
